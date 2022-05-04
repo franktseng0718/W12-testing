f55d5409a3065a02222badc4efd2027988281f3c
franktseng0718 <franktseng0718@gmail.com>
2022-05-04 20:59:43 +0800

    add test

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -5,3 +5,20 @@ describe('Game', () => {
     expect(new Game()).toBeTruthy();
   });
 });
+
+// game.spec.ts
+test('gutter game', () => {
+  const game = new Game();
+  for (let i = 0; i < 20; i++) {
+    game.roll(0);
+  }
+  expect(game.score).toBe(0);
+});
+
+test('all one', () => {
+  const game = new Game();
+  for (let i = 0; i < 20; i++) {
+    game.roll(1);
+  }
+  expect(game.score).toBe(20);
+});

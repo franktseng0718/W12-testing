c89c43f677ae2fc98fb931a13b83a7919feaae09
franktseng0718 <franktseng0718@gmail.com>
2022-05-05 15:40:32 +0800

    Green2

--- apps/bowling/src/app/book.ts
+++ apps/bowling/src/app/book.ts
@@ -2,10 +2,23 @@
 //book.ts
 export class Books {
   private price: number = 0;
+  private books = new Map();
   buy(k: number, n: number) {
+    let amount;
+    if(this.books.has(k)) {
+      amount = this.books.get(k) + n;
+    } else {
+      amount = 0 + n;
+    }
+    this.books.set(k, amount);
     this.price += 8 * n;
   }
   get total_price() {
-    return this.price;
+    let discount = 1;
+    if(this.books.size == 2) discount = 0.95;
+    if(this.books.size == 3) discount = 0.90;
+    if(this.books.size == 4) discount = 0.80; 
+    if(this.books.size == 5) discount = 0.75;
+    return this.price*discount;
   }
 }

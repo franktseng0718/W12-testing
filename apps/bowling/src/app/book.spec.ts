1543f664beee449f388e85f36e69024ef36f1106
franktseng0718 <franktseng0718@gmail.com>
2022-05-05 15:45:23 +0800

    Red3

--- apps/bowling/src/app/book.spec.ts
+++ apps/bowling/src/app/book.spec.ts
@@ -79,3 +79,42 @@ describe('testSimpleDiscounts', () => {
     expect(books.total_price).toBe(8*5*0.75);
   });
 });
+
+describe('testSeveralDiscounts', () => {
+  let books: Books;
+
+  beforeEach(() => {
+    books = new Books();
+  });
+  test('buy 0, 0, 1', () => {
+    books.buy(0, 1);
+    books.buy(0, 1);
+    books.buy(1, 1);
+    expect(books.total_price).toBe(8*2*0.95+8);
+  });
+
+  test('buy 0, 0, 1, 1', () => {
+    books.buy(0, 2);
+    books.buy(1, 2);
+    expect(books.total_price).toBe(8*2*0.95*2);
+  });
+
+  test('buy 0, 0, 1, 2, 2, 3', () => {
+    books.buy(0, 2);
+    books.buy(1, 1);
+    books.buy(2, 2);
+    books.buy(3, 1);
+    expect(books.total_price).toBe(8*4*0.8+8*2*0.95);
+  });
+
+  test('buy 0, 1, 1, 2, 3, 4', () => {
+    books.buy(0, 1);
+    books.buy(1, 2);
+    books.buy(2, 1);
+    books.buy(3, 1);
+    books.buy(4, 1);
+    expect(books.total_price).toBe(8*5*0.75+8);
+  });
+});
+
+
